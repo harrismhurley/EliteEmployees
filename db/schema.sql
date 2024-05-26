393189b43b3de0af01f29e9f90284bf5dc2884e0
harrismhurley <harrismhurley@icloud.com>
2024-05-26 03:30:40 -0500

    edited the schema and added some basic seeds

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,13 @@
+-- @block
 -- Drop the database if it exists
-DROP DATABASE IF EXISTS employeetrack_db;
-
+DROP DATABASE IF EXISTS company_db;
 -- Create the database
-CREATE DATABASE employeetrack_db;
-
+CREATE DATABASE company_db;
 -- Connect to the newly created database
-\c employeetrack_db
+\c company_db
 
+
+-- @block
 -- Create the department table
 CREATE TABLE IF NOT EXISTS department (
     id SERIAL PRIMARY KEY,
@@ -14,14 +15,18 @@ CREATE TABLE IF NOT EXISTS department (
 );
 
 -- Create the role table
-CREATE TABLE IF NOT EXISTS role (
+CREATE TABLE IF NOT EXISTS roles (
     id SERIAL PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INTEGER,
-    FOREIGN KEY (department_id)
-        REFERENCES department(id)
-        ON DELETE SET NULL
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+);
+
+-- Create the managers table
+CREATE TABLE IF NOT EXISTS managers (
+    manager_name VARCHAR(55),
+    manager_id SERIAL PRIMARY KEY
 );
 
 -- Create the employees table
@@ -30,7 +35,7 @@ CREATE TABLE IF NOT EXISTS employees (
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INTEGER,
-    FOREIGN KEY (role_id)
-        REFERENCES role(id)
-        ON DELETE SET NULL
+    manager_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
+    FOREIGN KEY (manager_id) REFERENCES managers(manager_id) ON DELETE SET NULL
 );
